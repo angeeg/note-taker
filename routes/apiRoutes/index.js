3621c24aae955f7c7a272cd1f7cd48139776cb64
Angeleena <angeleenasyse@gmail.com>
2021-07-15 18:55:13 -0700

    Add Heroku/Initial app setup

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const { findById, createNewNote, validateNote } = require("../../lib/notes");
+const { notes } = require("../../db/db.json");
+
+
+
+router.get("/notes", (req, res) => {
+  let results = notes;
+
+  res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+  res.json(result);
+} else {
+    res.send(404);
+}
+});
+
+router.post("/notes", (req, res) => {
+  // req.body is where our incoming content will be
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    // add note to json file and animals array in this function
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
